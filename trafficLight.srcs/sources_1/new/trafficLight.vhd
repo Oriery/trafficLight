7ca86aac7460ba54cea93f28d74c42a1383731ee
Oriery <panev291002@gmail.com>
2023-11-20 18:06:08 +0300

    git init

---
+++ trafficLight.srcs/sources_1/new/trafficLight.vhd
@@ -0,0 +1,47 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 20.11.2023 17:56:02
+-- Design Name: 
+-- Module Name: trafficLight - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity trafficLight is
+    Port ( IsOn : in STD_LOGIC;
+           Clk : in STD_LOGIC;
+           Red : out STD_LOGIC;
+           Yellow : out STD_LOGIC;
+           Green : out STD_LOGIC);
+end trafficLight;
+
+architecture Behavioral of trafficLight is
+
+begin
+
+
+end Behavioral;
